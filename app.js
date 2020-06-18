0dacd30fae19104ffd120f9186adb25605e27ccc
Mariaqu98 <mariaqu48@gmail.com>
2020-06-18 17:20:14 +0300

    frontend++somelogic

---
+++ app.js
@@ -0,0 +1,9 @@
+var box = document.getElementsByTagName("td")
+var counter = 0 
+box.onclick = function click (){
+// if the player will start with X then the next click will be O , so show X when the counter is Even , O when its Odd 
+console.log(5)
+
+};
+
+
